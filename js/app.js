f71ce375554a849e1f48b7d5718c45ce41ae2899
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-02-17 17:58:07 -0500

    Added populateDropdown code

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ function Animal(url, title, description, keyword, horns){
 
 Animal.prototype.renderAnimal = function(){
   const $liBase = $('<div></div>');
-  console.log($liBase);
   const $h2 = $('<h2></h2>').text(this.title);
   const $image = $('<img></img>').attr('src', this.url);
   const $p = $('<p></p>').text(this.description);
@@ -26,20 +25,36 @@ $.ajax('data/page-1.json',{
   success:function (response){
     console.log('it works!');
     extractJsonData(response);
+    populateDropdown(response);
   },
   error: function (req, status, error){
     console.log('it\'s broken', status, error);
   }
 });
+
+
 function extractJsonData(jsonInfo){
   console.log(typeof(jsonInfo));
   jsonInfo.forEach(animal => {
     new Animal (animal.image_url, animal.title, animal.description, animal.keyword, animal.horns).renderAnimal();
-  }
-  );
+  });
   //Animal.allAnimals.forEach(animalEntry => animalEntry.renderAnimal());
 }
 
+const currentOptions = {};
 
-
-
+function populateDropdown(jsonInfo){
+  
+  jsonInfo.forEach(animal =>{
+    let optionText = animal.keyword.toString().charAt(0).toUpperCase() + animal.keyword.slice(1);
+    let optionValue = animal.keyword;
+    const $option = new Option(optionText,optionValue);
+    currentOptions[animal.keyword] = true;
+  });  
+  const objKeys = Object.keys(currentOptions);
+  for(let key in objKeys)
+  {
+    let optionText = objKeys[key].toString().charAt(0).toUpperCase() + objKeys[key].slice(1);
+    $('select').append(new Option(optionText,objKeys[key]));
+  }
+}
