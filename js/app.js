4c3270695c1510b0b67716a61cf5a5e89e456fa2
Andy Agulue <andyfandandy@gmail.com>
2021-02-16 19:26:47 -0800

    lab 2 push 2

--- js/app.js
+++ js/app.js
@@ -6,24 +6,36 @@ function Animal(url, title, description, keyword, horns){
   this.description = description;
   this.keyword = keyword;
   this.horns = horns;
+  console.log(this);
   Animal.allAnimals.push(this);
 }
 Animal.allAnimals = [];
-
 Animal.prototype.renderAnimal = function(){
   const $liBase = $('li:first-child').clone();
   $liBase.find('h2').text(this.title);
   $liBase.find('img').attr('src', this.url);
   $liBase.find('p').text(this.description);
+  console.log('renderanimal');
   $('ul').append($liBase);
 };
 
-$.ajax('data/page-1.json').then(extractJsonData);
+
+$.ajax('data/page-1.json',{
+  success:function (response){
+    console.log('it works!');
+    extractJsonData(response);
+  },
+  error: function (req, status, error){
+    console.log('it\'s broken', status, error);
+  }
+});
 function extractJsonData(jsonInfo){
-  console.log(jsonInfo);
+  console.log(typeof(jsonInfo));
   jsonInfo.forEach(animal => {
-    new Animal (animal.url, animal.title, animal.description, animal.keyword, animal.horns);
-  });
+    let dog = new Animal (animal.url, animal.title, animal.description, animal.keyword, animal.horns);
+    console.log(dog);
+  }
+  );
   Animal.allAnimals.forEach(animalEntry => animalEntry.renderAnimal());
 }
 
